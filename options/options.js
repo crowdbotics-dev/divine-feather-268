fb75b77b5549af1dd8aac0346116f4900c359bcb
Crowdbotics <engineering@crowdbotics.com>
2024-04-02 07:13:11 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "divine-feather-268",
+  url: "https://divine-feather-268.botics.co",
+  api: "https://divine-feather-268.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
